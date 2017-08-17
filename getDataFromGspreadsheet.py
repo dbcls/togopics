82daf72765c352f96c626c923db3420ad537e4ac
Y Yamamoto <yy@dbcls.rois.ac.jp>
2017-08-17 17:04:52 +0900

    Google Spreadsheetの統合TV原簿から直接データを取得してCSV形式で標準出力に出力するgetDataFromGspreadsheet.pyno

---
+++ getDataFromGspreadsheet.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+import sys
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import json
+import unicodecsv as csv
+#import codecs
+
+#sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+sheet = client.open(u"統合TV番組原簿 20150701").worksheet("Pictures")
+
+writer = csv.writer(sys.stdout)
+header = sheet.row_values(1)[0:19]
+writer.writerow(header)
+list_of_hashes = sheet.get_all_records()
+
+for v in [[row[colname] for colname in header] for row in list_of_hashes]:
+  writer.writerow(v)
+#print json.dumps(list_of_hashes)
