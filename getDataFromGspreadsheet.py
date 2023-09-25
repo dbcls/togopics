fff5618bf0c53f007dbb8cb7eeb04a69ab5cc606
Yasunori Yamamoto <yy@dbcls.rois.ac.jp>
2023-09-25 21:25:09 +0900

    Modified to support the latest python3.

--- getDataFromGspreadsheet.py
+++ getDataFromGspreadsheet.py
@@ -4,10 +4,7 @@ import sys
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
-import unicodecsv as csv
-#import codecs
-
-#sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+import csv
 
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
@@ -15,13 +12,10 @@ client = gspread.authorize(creds)
 # sheet = client.open(u"統合TV番組原簿 20150701").worksheet("Pictures")
 sheet = client.open(u"2020統合TV番組原簿").worksheet("Pictures")
 
-#writer = csv.writer(sys.stdout)
-writer = csv.writer(sys.stdout.buffer)
+writer = csv.writer(sys.stdout)
 header = sheet.row_values(1)[0:31]
-#writer.writerow(header)
-writer.writerow([str(val).encode('utf-8') for val in header])
+writer.writerow(header)
 list_of_hashes = sheet.get_all_records()
 
 for v in [[row[colname] for colname in header] for row in list_of_hashes]:
   writer.writerow(v)
-#print json.dumps(list_of_hashes)
